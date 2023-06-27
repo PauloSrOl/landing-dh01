d98959f779f10fb2c3b69ba7ad86ac7daf20d2ac
Paulo Soares <paulosr870@gmail.com>
2023-06-27 20:39:35 -0300

    geral.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+/*============================ tooogle icon navbar =============================*/
+let menuIcon = document.querySelector('#menu-icon');
+let naavbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+/*============================ scroll sections ctive link =============================*/
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a')
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -15,4 +25,15 @@ window.onscroll = () => {
             });
         };
     });
-}
+
+    /*============================ sticky nvbr =============================*/
+        let header = document.querySelector('header')
+
+        header.classList.toggle('sticky', window.scrollY > 100);
+
+
+/*========================= remove tooogle icon navbar =============================*/   
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+
+};
